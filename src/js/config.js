2e89ac0dde8be72f8efaad5d4ec3864d9d4e1529
samincgs <samincgs@gmail.com>
2023-05-02 05:04:38 -0400

    Initial Commit

---
+++ src/js/config.js
@@ -0,0 +1,6 @@
+//all constant variables
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 15;
+export const KEY = 'a341de73-f9e2-4d99-97d7-ff99f3ae0da1';
+export const MODAL_CLOSE_SEC = 2.5;
